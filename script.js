aa300e1fc6adffd58775e4ce94a8f1db840296d2
Drelka <47503020+Drelka@users.noreply.github.com>
2025-10-20 00:59:58 +0100

    Add comments to js
    Had to explain myself how it works step-by-step.

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const wasItRead = document.querySelector("#read");
 const buttonAdd = document.querySelector("#button-add")
 const library = document.querySelector(".library");
 
+// Book constructor (a function) creating a Book object with it's properties like "author" and "title"
+
 function Book(title, author, pages, wasItRead) {
     if(!new.target) {
         throw Error("Please use the \"new\" keyword with object constructors!");
@@ -18,6 +20,12 @@ function Book(title, author, pages, wasItRead) {
     this.wasItRead = wasItRead;
 }
 
+// function adding book to the library by:
+// 1. creating variables (const) and passing them values from the HTML input fields (given by the user)
+// 2. creating new Book object (using the above seen object constructor), then passing it values from previously created variables
+//    in form of functions arguments
+// 3. pushing new Book object (named newBook) into the myLibrary array
+
 function addBookToLibrary() {
     const bookTitle = title.value;
     const bookAuthor = author.value;
@@ -28,6 +36,14 @@ function addBookToLibrary() {
 
     myLibrary.push(newBook);
 }
+
+// function "printing" the library on the screen:
+// 1. clears the HTML code of a library DOM object
+// 2. performs tasks on every element of the myLibrary array:
+//   - creates new div element (gives it a "div" name) and adds a ".library" class to it's class list
+//   - creates new paragraphs for all 4 book properties and sets its textContent to "key" + "value"
+// 3. appends all 4 newly created paragraphs to the newly created div
+// 4. appends new div to the library DOM element
 
 function printBook() {
     library.innerHTML = "";
@@ -51,9 +67,11 @@ function printBook() {
     });
 }
 
+// button, with on click action that calls 2 above functions
+
 buttonAdd.addEventListener("click", (e) => {
     e.preventDefault();
+    
     addBookToLibrary();
-    console.log(myLibrary);
     printBook();
 });
